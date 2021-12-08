c63a6ff5ae9909b932882a8be97c6a96a65c6d9c
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 04:10:25 -0500

    Update state.tf

--- deployment_infrastructure/state.tf
+++ deployment_infrastructure/state.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-   bucket         = "infras-s3-state-55"
+   bucket         = "infras-s33-state-final"
    key            = "state/terraform.tfstate"
    region         = "us-east-1"
    encrypt        = true
