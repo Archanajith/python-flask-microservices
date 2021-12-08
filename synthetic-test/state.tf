c6a5f1e26db1929dd558b771efcc5f1ae41d1313
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 13:14:14 -0500

    Update state.tf

--- synthetic-test/state.tf
+++ synthetic-test/state.tf
@@ -2,7 +2,7 @@ terraform {
  backend "s3" {
    bucket         = "synthetic-s3-state-46"
    key            = "state/terraform.tfstate"
-   region         = "us-east-1"
+   region         = "us-east-2"
    encrypt        = true
    kms_key_id     = "alias/terraform-synthetic-s3-key-46"
    dynamodb_table = "terraform-synthetic-state-46"
