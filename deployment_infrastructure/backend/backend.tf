c520e6002e0cadbf2caa640028414bed1beb0f7b
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 04:16:36 -0500

    Update backend.tf

--- deployment_infrastructure/backend/backend.tf
+++ deployment_infrastructure/backend/backend.tf
@@ -4,19 +4,19 @@ provider "aws" {
   
 }
 
-resource "aws_kms_key" "terraform-infras-bucket-key-101" {
+resource "aws_kms_key" "terraform-infras-microservices-bucket-key-101" {
  description             = "This key is used to encrypt bucket object"
  deletion_window_in_days = 10
  enable_key_rotation     = true
 }
 
 resource "aws_kms_alias" "terraform-infra-key-alias" {
- name          = "alias/terraform-infras-bucket-key-101"
- target_key_id = aws_kms_key.terraform-infras-bucket-key-101.key_id
+ name          = "alias/terraform-infras-microservices-bucket-key-101"
+ target_key_id = aws_kms_key.terraform-infras-microservices-bucket-key-101.key_id
 }
 
-resource "aws_s33_bucket" "terraform-infras-state-final" {
- bucket = "infras-s33-state-final"
+resource "aws_s3_bucket" "terraform-infras-state-final-project" {
+ bucket = "infras-microservices-s33-state-final"
  acl    = "private"
 
  versioning {
@@ -26,7 +26,7 @@ resource "aws_s33_bucket" "terraform-infras-state-final" {
  server_side_encryption_configuration {
    rule {
      apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.terraform-infras-bucket-key-101.arn
+       kms_master_key_id = aws_kms_key.terraform-infras-microservices-bucket-key-101.arn
        sse_algorithm     = "aws:kms"
      }
    }
@@ -34,7 +34,7 @@ resource "aws_s33_bucket" "terraform-infras-state-final" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block-infra" {
- bucket = aws_s33_bucket.terraform-infras-state-final.id
+ bucket = aws_s33_bucket.terraform-infras-state-final-project.id
 
  block_public_acls       = true
  block_public_policy     = true
@@ -42,8 +42,8 @@ resource "aws_s3_bucket_public_access_block" "block-infra" {
  restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform-infras-state-final" {
- name           = "terraform-infras-state-final"
+resource "aws_dynamodb_table" "terraform-infras-state-final-project" {
+ name           = "terraform-infras-state-final-project"
  read_capacity  = 20
  write_capacity = 20
  hash_key       = "LockID"
