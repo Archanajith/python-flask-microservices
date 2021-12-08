3a1fc36b4440882c450db3d2797355e585995105
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 13:02:28 -0500

    Update state.tf

--- monitoring/state.tf
+++ monitoring/state.tf
@@ -1,10 +1,10 @@
 terraform {
  backend "s3" {
-   bucket         = "monitor-s3-state-1"
+   bucket         = "monitor-s3-state-44"
    key            = "state/terraform.tfstate"
    region         = "us-east-2"
    encrypt        = true
    kms_key_id     = "alias/terraform-monitor-bucket-key-44"
-   dynamodb_table = "terraform-monitor-state-1"
+   dynamodb_table = "terraform-monitor-state-44"
  }
 }   
