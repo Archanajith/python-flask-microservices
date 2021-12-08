53962645311c4bf4e9c479c19ff6bef10884cc21
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 13:13:32 -0500

    Update variable.tf

--- synthetic-test/backend/variable.tf
+++ synthetic-test/backend/variable.tf
@@ -1,12 +1,12 @@
 variable "s3_bucket" {
   type        = string
-  default     = "synthetic-s3-state-1"
+  default     = "synthetic-s3-state-46"
   description = "The s3 bucket name"
 }
 
 variable "dynamo_db_table" {
   type        = string
-  default     = "terraform-synthetic-state-11"
+  default     = "terraform-synthetic-state-46"
   description = "Name of dynamo db table"
 }
 
