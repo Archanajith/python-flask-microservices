a4fc753c53c52fd932edd80e8a3e040ccb7efde8
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 12:06:50 -0500

    Update state.tf

--- deployment_infrastructure/state.tf
+++ deployment_infrastructure/state.tf
@@ -2,7 +2,7 @@ terraform {
  backend "s3" {
    bucket         = "infra-s3-state-34"
    key            = "state/terraform.tfstate"
-   region         = "us-east-1"
+   region         = "us-east-2"
    encrypt        = true
    kms_key_id     = "alias/terraform-infra-bucket-key-34"
    dynamodb_table = "terraform-infra-state-34"
