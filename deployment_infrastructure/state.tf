2b5e9f058316f06f2ccc0e15dbc5f06ac907416a
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 04:09:49 -0500

    Update state.tf

--- deployment_infrastructure/state.tf
+++ deployment_infrastructure/state.tf
@@ -5,6 +5,6 @@ terraform {
    region         = "us-east-1"
    encrypt        = true
    kms_key_id     = "alias/terraform-infras-bucket-key-101"
-   dynamodb_table = "terraform-infras-state-55"
+   dynamodb_table = "terraform-infras-state-final"
  }
 }   
